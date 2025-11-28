4666cdd2849f9476efdf1a13c20276b59e7d79c7
renanpearce <pearce2renan@gmail.com>
2025-11-28 15:01:22 -0300

    Update app_sqlite.py

--- app_sqlite.py
+++ app_sqlite.py
@@ -10,5 +10,67 @@ O que o script deve fazer:
 5) Buscar por id
 6) Atualizar registros
 7) Deletar registros
-
 """
+import sqlite3
+
+conn = sqlite3.connect('escola.db')
+cursor = conn.cursor()
+
+# passo 2 - criar tabelas
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS alunos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER,
+    email Text
+)
+""")
+
+print('Tabela criada com sucesso!\n')
+
+# passo 3 -inserir dados
+
+cursor.execute('INSERT INTO alunos(nome, idade, email) VALUES(?,?,?)',
+               ('jose',18, 'jose@gmail.com'))
+cursor.execute('INSERT INTO alunos(nome, idade, email) VALUES(?,?,?)',
+               ('Matheus',17, 'matheus@gmail.com'))
+cursor.execute('INSERT INTO alunos(nome, idade, email) VALUES(?,?,?)',
+               ('Gustavo',17, 'gustavo@gmail.com'))
+cursor.execute('INSERT INTO alunos(nome, idade, email) VALUES(?,?,?)',
+               ('Diego',18, 'diego@gmail.com'))
+conn.execute('INSERT INTO alunos(nome, idade, email) VALUES(?,?,?)',
+               ('Wellington',17, 'wellignton@gmail.com'))
+conn.commit()
+
+print("Dados inseridos!\n")
+
+# Passo 4 - Listar todos
+print("Lista de alunos cadastrados:")
+cursor.execute('SELECT * FROM alunos')
+for linha in cursor.fetchall():
+    print(linha)
+print()
+
+# Passo 5 - Atualizar um registro
+cursor.execute('UPDATE alunos SET email = ? WHERE nome = ?',
+               ('jose.dev@gmail.com', 'José' ))
+
+conn.commit()
+print('Após atualização do email do José:')
+cursor.execute('SELECT * FROM alunos')
+for linha in cursor.fetchall():
+    print(linha)
+print()
+
+# Passo 6 - Deletar um registro
+cursor.execute('DELETE FROM alunos WHERE nome = ?', ('Gustavo',))
+conn.commit()
+
+print('Após deletar o email do Gustavo:')
+cursor.execute('SELECT * FROM alunos')
+for linha in cursor.fetchall():
+    print(linha)
+print()
+
+# encerrar conecxao
+conn.close()
